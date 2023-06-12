49be81cad4d9c8e86f81a1aecdbe72d95e12c69a
Anderson B.O.B <55605767+eusouanderson@users.noreply.github.com>
2023-06-11 22:12:09 -0300

    "update"

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Lista de Tarefas</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={newTask} onChange={handleInputChange} />
+        <button type="submit">Adicionar</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
